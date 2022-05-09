3568bff8c5962b127ccbc8635053227459eb7ac8
annomao <omaoannkerubo@gmail.com>
2022-05-09 22:16:17 +0300

    Completed assignment

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -9,9 +9,11 @@ function MoviesPage({ movies }) {
   return (
     <div>
       <MoviesList movies={movies} />
+
       <Route exact path={match.url}>
         <h3>Choose a movie from the list above</h3>
       </Route>
+      
       <Route path={`${match.url}/:movieId`}>
         <MovieShow movies={movies} />
       </Route>
